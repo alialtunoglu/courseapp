311de36a7235c55271d69eee118b1ac7244dfd10
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-10-23 17:44:17 +0300

    add template

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
@@ -6,21 +7,56 @@ from django.urls import reverse
 data = {
      "programlama":"programlama kategorisine ait kurslar",
      "web-gelistirme":"web geliştirme kategorisine ait kurslar",
-     "mobil":"mobil kategorisine ait kurslar"
+     "mobil-uygulamalar":"mobil kategorisine ait kurslar",
 }
+db = {
+     "courses":[
+          {
+               "title":"javascript kursu",
+               "description":"javascript kurs açıklaması",
+               "imageUrl":"https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+               "slug":"javascript-kursu",
+               "date":date(2022,10,10),
+               "isActive":True,
+               "isUpdated":False
+          },
+          {
+               "title":"python kursu",
+               "description":"python kurs açıklaması",
+               "imageUrl":"https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+               "slug":"python-kursu",
+               "date":date(2022,9,10),
+                "isActive":False,
+               "isUpdated":False
+          },
+          {
+               "title":"web geliştirme kursu",
+               "description":"web geliştirme kurs açıklaması",
+               "imageUrl":"https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+               "slug":"web-gelistirme-kursu",
+               "date":date(2022,8,10),
+               "isActive":True,
+               "isUpdated":True
+          }
+     ],
+     "categories":[
+          {"id": 1, "name":"programlama","slug":"programlama"},
+          {"id": 2, "name":"web geliştirme","slug":"web-gelistirme"},
+          {"id": 3, "name":"mobil uygulamalar","slug":"mobil-uygulamalar"},
+    
+          ]
+}
+
+
 # http://127.0.0.1:8000/kurslar
-def index (request):
-     return render(request,'courses/index.html')
-def kurslar(request):
-    list_items = ""
-    category_list = list(data.keys())
+def index(request):
+    kurslar = [course for course in db['courses'] if course['isActive']]
+    kategoriler = db['categories']
 
-    for category in category_list:
-        redirect_url=reverse('courses_by_category',args=[category])
-        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
-    html = f"<h1> Kurs Listesi </h1><br><ul>{list_items}</ul>"
-
-    return HttpResponse(html)
+    return render(request,'courses/index.html', {
+         'categories':kategoriler,
+         'courses':kurslar,
+    })  
 
 def details(request,kurs_adi):  
         return HttpResponse(f'{kurs_adi} detay sayfası')
@@ -28,7 +64,10 @@ def details(request,kurs_adi):
 def getCoursesByCategory(request,category_name):
     try:
         category_text=data[category_name]
-        return HttpResponse(category_text)
+        return render(request,'courses/kurslar.html',{
+             'category':category_name,
+             'category_text':category_text,
+        })
     except:
         return HttpResponseNotFound("<h1>yanlış kategori seçimi</h1>")     
 
