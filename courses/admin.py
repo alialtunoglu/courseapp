53b090c8a670968dd7bdac775d40023c86379346
Ali Altunoğlu <alialtunoglu02@gmail.com>
2024-10-26 12:14:06 +0300

    add admin panels

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Course,Category
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title","isActive","slug")
+    list_display_links = ("title","slug")
+    readonly_fields = ("slug",) #readonly alanlar
+    list_filter = ("title","isActive")
+    list_editable = ("isActive",) #editable alanlar
+    search_fields = ("title","description") #search alanları
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+# admin.site.register(Course,CourseAdmin) #Course modelini admin paneline ekledik.
+# admin.site.register(Category) #Category modelini admin paneline ekledik.
+
+
--- courses/models.py
+++ courses/models.py
@@ -7,15 +7,19 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="",null=False,unique=True,db_index=True) #php-kursu
-    
+    slug = models.SlugField(default="",blank=True,editable=False,null=False,unique=True,db_index=True) #php-kursu
+    #editable = admin panelinde bu alan formda görünmesin
+    #blank = boş değer olabilir admin sayfasındaki form yapısında, null = boş olamaz vt'de
     def save(self,*args,**kwargs):
-        self.slug = slugify(self.title)
+        self.slug = slugify(self.title)  
         super().save(*args,**kwargs)
 
     def __str__(self):
-        return f"{self.title} {self.date}"
+        return f"{self.title}"
 
 class Category (models.Model):
     name= models.CharField(max_length=40)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
