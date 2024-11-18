f504929eaec73578cd30ae4a7687e157e100a50f
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-11-18 17:46:33 +0300

    add login_processing

---
+++ account/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+# Create your views here.
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            return render(request, 'account/login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'account/login.html')
+        
+def user_register(request):
+    return render(request, 'account/register.html')
+def user_logout(request):
+    logout(request)
+    return redirect('index')
