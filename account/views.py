b43e58cc9ee9375fca313553796f7f7b3c506454
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-11-21 16:18:35 +0300

    add authenticationform

--- account/views.py
+++ account/views.py
@@ -2,57 +2,70 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-
-
+from account.forms import LoginUserForm, NewUserForm, UserPasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 def user_login(request):
     if request.user.is_authenticated and  "next" in request.GET:
         return render(request, 'account/login.html', {'error': 'Yetkisiz kullanıcı erişimi'})
 
     if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
+        form = LoginUserForm(request,data=request.POST)
+        if form.is_valid():
 
-        user = authenticate(request,username=username, password=password)
-        
-        if user is not None:
-            login(request,user)
-            messages.add_message(request,messages.SUCCESS,"Giriş Başarılı")
-            nextUrl= request.GET.get("next",None)
-            if nextUrl is None:
-                return redirect('index')
+            username=form.cleaned_data.get("username")
+            password=form.cleaned_data.get("password")
+            
+
+            user = authenticate(request,username=username, password=password)
+
+            if user is not None:
+                login(request,user)
+                messages.add_message(request,messages.SUCCESS,"Giriş Başarılı")
+                nextUrl= request.GET.get("next",None)
+                if nextUrl is None:
+                    return redirect('index')
+                else:
+                    return redirect(nextUrl)
             else:
-                return redirect(nextUrl)
+                return render(request,"account/login.html",{"form":form})
         else:
-            messages.add_message(request,messages.ERROR,"Parola veya username yanlış!")
-            return render(request, 'account/login.html')
+            return render(request, 'account/login.html',{"form":form})
     else:
-        return render(request, 'account/login.html')
+        form = LoginUserForm()
+        return render(request, 'account/login.html',{"form":form})
         
 def user_register(request):
     if request.method == 'POST':
-        username=request.POST['username']
-        email=request.POST['email']
-        password=request.POST['password']
-        repassword=request.POST['repassword']
-        if password != repassword:
-            return render(request, 'account/register.html', {'error': 'Parola eşleşmiyor','username':username,'email':email})
-        if User.objects.filter(username=username).exists():
-            return render(request, 'account/register.html', {'error': 'Bu username daha önce kullanılmış!','username':username,'email':email})
-        
-        if User.objects.filter(email=email).exists():
-            return render(request, 'account/register.html', {'error': 'Bu email daha önce kullanılmış!','username':username,'email':email})
-        
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
-        # Kullanıcıyı otomatik giriş yaptır
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)  # Kullanıcıyı giriş yaptır
-            return redirect('index')  # Ana sayfaya yönlendir    
-    else:
-        return render(request, 'account/register.html')
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
 
+            username=form.cleaned_data["username"]
+            password=form.cleaned_data["password1"]
+            user = authenticate(request,username=username, password=password)
+            login(request,user)
+            return redirect('index')
+        else:
+            return render(request,'account/register.html',{'form':form})
+    else:
+        form = NewUserForm()
+        return render(request, 'account/register.html',{"form": form})
+
+
+def change_password(request):
+    if request.method == "POST":
+        form = UserPasswordChangeForm(request.user,request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.add_message(request,messages.SUCCESS,"Şifreniz Başarıyla Değiştirildi")
+            return redirect('change_password')
+        else:
+            return render(request, 'account/change-password.html',{"form":form})
+    form= UserPasswordChangeForm(request.user)
+    return render(request, 'account/change-password.html',{"form":form})
     
+
 def user_logout(request):
     messages.add_message(request,messages.SUCCESS,"Çıkış Başarılı")
     logout(request)
