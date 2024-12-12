d6898d116939c21577ac8e4d9c5341af001a2fed
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-12-12 16:15:23 +0300

    ending

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -89,15 +89,10 @@ WSGI_APPLICATION = "courseapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "courseapp_db_postgresql"),
-        "USER": os.getenv("DB_USER", "courseapp_db_postgresql_user"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "1PJMZXTBiVCA42w7EZKsG1AliShPq65A"),
-        "HOST": os.getenv("DB_HOST", "dpg-ctdcvjggph6c73erhoig-a"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 
 # Password validation
