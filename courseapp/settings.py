0b5b72d3d7c60eb51f9ee441599acf32d2ff2829
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-12-12 15:02:09 +0300

    added postgresql

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = "courseapp.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'courseapp_db_postgresql',
+        'USER': 'courseapp_db_postgresql_user',
+        'PASSWORD': '1PJMZXTBiVCA42w7EZKsG1AliShPq65A',
+        'HOST': 'dpg-ctdcvjggph6c73erhoig-a',
+        'PORT': '5432',
     }
 }
 
